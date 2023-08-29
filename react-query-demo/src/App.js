cf56773237155b81d740db9b4e3b176e1a11cd6e
Thanatat Wongabut <thanatat.wong@mail.kmutt.ac.th>
2023-08-29 21:53:44 +0700

    Finished Fetching Data with useQuery

---
+++ react-query-demo/src/App.js
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ResponsiveAppBar from "./components/Nav";
+import { HomePage } from "./components/Home.page";
+import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
+import { SuperHeroesPage } from "./components/SuperHeroes.page";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <div className="App">
+      <QueryClientProvider client={queryClient}>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/HomePage" element={<HomePage />} />
+          <Route path="RQSuperHeroes" element={<RQSuperHeroesPage />} />
+          <Route path="SuperHeroes" element={<SuperHeroesPage />} />
+        </Routes>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default App;
